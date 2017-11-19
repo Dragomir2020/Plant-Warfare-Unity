c6d77a4d0ffbcbcd65c6516821d5ae9438ca4041
dragomdv <dragdi15@gmail.com>
2017-11-19 18:17:08 -0500

    Create menu system and persistent music manager

---
+++ PlantWarfare/Assets/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	/// <summary>
+	/// Loads new music for each level
+	/// </summary>
+	/// <param name="level">Level.</param>
+	void OnLevelWasLoaded(int level){
+		AudioClip music = levelMusicChangeArray [level];
+		//If music is attached in 
+		if (music != null){
+			audioSource.clip = music;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
---
+++ PlantWarfare/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+/// <summary>
+///  Class handles level related tasks as well as switching scenes
+/// </summary>
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevel;
+
+	void Start(){
+		Debug.LogWarning ("Yasss");
+		if (autoLoadNextLevel != 0f) {
+			Invoke ("LoadNextLevel", autoLoadNextLevel);
+		}
+	}
+
+	/// <summary>
+	///  Open new scenes
+	/// </summary>
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	/// <summary>
+	///  Quit game
+	/// </summary>
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		SceneManager.LoadScene ("StartMenu");
+	}
+
+}
