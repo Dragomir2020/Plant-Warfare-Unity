1b57b5c05c645a7265bd42b4fa83222adba9a679
dragomdv <dragdi15@gmail.com>
2017-12-15 12:31:40 -0500

    Finish adding all character animations

---
+++ PlantWarfare/Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+	private MusicManager musicManager;
+
+	// Find Music manager and set volume
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+	}
+
+}
